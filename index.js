8983097c5e59c05497cffa543bd8cf1a1d55c57b
mdAshik922 <md.ashik0005723@gmail.com>
2021-12-12 14:30:06 +0600

    commit

--- index.js
+++ index.js
@@ -95,21 +95,30 @@ app.delete('/order/:id', async(req, res) =>{
   res.json(campOrder);
 });
 
+// app.put('/users/admin',  async (req, res) => {
+//   const user = req.body;
+//   const requester = req.decodedEmail;
+//   if (requester) {
+//       const requesterAccount = await usersCollection.findOne({ email: requester });
+//       if (requesterAccount.role === 'admin') {
+//           const filter = { email: user.email };
+//           const updateDoc = { $set: { role: 'admin' } };
+//           const result = await usersCollection.updateOne(filter, updateDoc);
+//           res.json(result);
+//       };
+//   }
+//   else {
+//       res.status(403).json({ message: 'you do not have access to make admin' })
+//   }
+
+// });
 app.put('/users/admin',  async (req, res) => {
   const user = req.body;
-  const requester = req.decodedEmail;
-  if (requester) {
-      const requesterAccount = await usersCollection.findOne({ email: requester });
-      if (requesterAccount.role === 'admin') {
           const filter = { email: user.email };
           const updateDoc = { $set: { role: 'admin' } };
           const result = await usersCollection.updateOne(filter, updateDoc);
           res.json(result);
-      };
-  }
-  else {
-      res.status(403).json({ message: 'you do not have access to make admin' })
-  }
+     
 
 });
 
