1c59cb27f37d0eebbde85bea05a382137974fbec
mdAshik922 <md.ashik0005723@gmail.com>
2022-01-02 13:33:52 +0600

    commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
 
-//MONGODB CONECT
+//MONGODB connect
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.89jki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
